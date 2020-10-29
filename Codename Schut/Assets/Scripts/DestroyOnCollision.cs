64df7ddb39578cfb825a502c1b66f32d8cb01418
JCDM122300 <55261863+JCDM122300@users.noreply.github.com>
2020-10-29 01:35:57 -0500

    More features added
    Added the first game over function to the game, made random enemy spawn locations functional, and added some new collision functions.

---
+++ Codename Schut/Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //One thing I need to do is figure out how to get the bullet to ignore the player since it destroys it too
+        Debug.Log("Game Over");
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
--- Codename Schut/Assets/Scripts/EnemySpawner.cs
+++ Codename Schut/Assets/Scripts/EnemySpawner.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    private float spawnRangeX = 16.5f;
+    private float spawnPosZ = 20;
+    private float spawnPosY = 3.25f;
+    private float StartDelay = 2;
+    private float SpawnInterval = 1.5f;
+    public GameObject[] EnemyPrefabs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomEnemy", StartDelay, SpawnInterval);
     }
 
     // Update is called once per frame
@@ -15,4 +21,10 @@ public class EnemySpawner : MonoBehaviour
     {
         
     }
+        void SpawnRandomEnemy()
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY, spawnPosZ);
+            int EnemyIndex = Random.Range(0, EnemyPrefabs.Length);
+            Instantiate(EnemyPrefabs[EnemyIndex], spawnPos, EnemyPrefabs[EnemyIndex].transform.rotation);
+        }
 }
