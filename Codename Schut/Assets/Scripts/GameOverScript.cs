41334dae230ca14949b4e6dec1de0d80470c17ab
JCDM122300 <55261863+JCDM122300@users.noreply.github.com>
2020-11-11 23:04:16 -0600

    Added music track

--- Codename Schut/Assets/Scripts/GameOverScript.cs
+++ Codename Schut/Assets/Scripts/GameOverScript.cs
@@ -5,14 +5,5 @@ using UnityEngine;
 public class GameOverScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static bool IsGameOver = false;
 }
